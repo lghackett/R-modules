39d949db40a7cac870d228715c06401893c7afca
lucyhackett <lucyhackett23@gmail.com>
2022-01-17 10:39:24 -0800

    add eventstudy_formulator() function to reghelp module for creating feols forumulas for event studies

--- regHelpers/reghelp.R
+++ regHelpers/reghelp.R
@@ -21,3 +21,39 @@ feols_formulator <- function(outcome, xvec, fe){
                    paste(fe, collapse = ' + '))
   return(stats::as.formula(formula))
 }
+
+#' Event study formulator
+#' 
+#' Write formula for feols::feols() using a string for the outcome, a vector of
+#' strings for the X variables to facilitate iterating over output 
+#' variables in feols(), formatted in a way that iplot understands.
+#' @param outcome string. Yvariable in the regression
+#' @param eventvars vector of strings, event study FE's to be added. In the case of a 
+#' regular event study, these are usually event time and the treatment variable.
+#' In the case of the Sun and Abrahan (2020) estimator, these are the year of 
+#' treatment and the year variable.
+#' @param xvec vector of strings. Xvariables (excluding FE) in the regression
+#' @param fe The fixed effects to be added to the study. Usually include ID and 
+#' year FEs. 
+#' @param ref = c(-1): Reference years. May include more than 1 if there is a 
+#' never treated group.
+#' @param sunab = FALSE: A boolean for whether the Sun and Abrahan (2020) estimator
+#' should be calculated instead of a traditional event study.
+#' @return A \code{formula} appropriate for feols
+#' @export
+eventstudy_formulator <- function(outcome, eventvars, xvec, fe, ref=c(-1), sunab=F){
+  formula <- outcome
+  if(sunab){
+    formula <- paste(formula, ' ~ sunab(',
+                     paste(eventvars, collapse = ', '), ') + ',
+                     paste(xvec, collapse = ' + '), '|',
+                     paste(fe, collapse = ' + '))    
+  }else{
+    formula <- paste(formula, '~ i(',
+                     paste(eventvars, collapse = ' , '), ', ref = c(',
+                     paste(ref, collapse = ' , '), ')) + ',
+                     paste(xvec, collapse = ' + '), '|',
+                     paste(fe, collapse = ' + '))   
+  }
+  return(stats::as.formula(formula))
+}
